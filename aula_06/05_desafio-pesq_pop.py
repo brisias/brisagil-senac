8b98f177d5f40808be9c2f67af9748f8bc4be773
brisias <brisagils@gmail.com>
2025-04-07 14:30:41 -0300

    Add files via upload

--- aula_06/05_desafio-pesq_pop.py
+++ aula_06/05_desafio-pesq_pop.py
@@ -13,9 +13,28 @@ sexo = []
 olhos = []
 cabelos = []
 idade = []
+qtd_jovens_f = 0
+qtd_galegos = 0
 
-hab = int(input('Insira a quantidade de habitantes da região pesquisada: '))
+hab = int(input('Insira a quantidade de habitantes da região pesquisada: ')) #poderia inserir a cidade e puxar automaticamente de um banco de dados do IBGE, por exemplo, a população do município para determinar o tamanho do while (num futuro distante em que eu souber fazer isso).  
 n = 0
 
-while n <= hab:
-    sexo.append(input('Informe o sexo do indivíduo:'))
+while n < hab:
+    n += 1
+    sexo.append(input(f'\nRegistre o sexo do {n}º indivíduo. M - masculino ou F - feminino: ').upper())
+    olhos.append(input(f'Agora, registre a cor dos olhos do {n}º indivíduo. A - azuis; V - verdes ou; C - castanhos: ').upper())
+    cabelos.append(input(f'Registre também a cor dos cabelos do {n}º indivíduo. L - louros; C - castanhos ou; P - pretos: ').upper())
+    idade.append(int(input(f'Por fim, insira a idade do {n}º indivíduo: ')))
+    print()
+
+    for i in range(hab):
+        if sexo[i] == "F" and idade[i] >= 18 and idade[i] <= 35:
+            qtd_jovens_f += 1
+        if cabelos[i] == "L" and olhos[i] == "V":
+            qtd_galegos += 1
+
+maior = max(idade)
+
+print(f'\nA maior idade encontrada na população pesquisada foi: {maior} anos.')
+print(f'\nA quantidade de indivíduos do sexo feminino, entre 18 e 35 anos de idade, na população pesquisada, foi: {qtd_jovens_f} pessoa(s).')
+print(f'\nJá a quantidade de indivíduos com cabelos louros e olhos verdes, foi de: {qtd_galegos} galego(s).')
